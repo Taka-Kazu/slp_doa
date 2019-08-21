e5604c1f85bda82e20ce105b992a439628f41206
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-08-21 19:06:36 +0900

    update script

--- run_docker.sh
+++ run_docker.sh
@@ -5,9 +5,7 @@ echo "=== run_docker ==="
 python update_repositories.py
 
 docker run -it --rm \
-  --volume="${PWD}/slp_doa:/root/catkin_ws/src/slp_doa" \
-  --volume="${PWD}/dynamic_obstacle_avoidance_planner:/root/catkin_ws/src/dynamic_obstacle_avoidance_planner" \
-  --volume="${PWD}/state_lattice_planner:/root/catkin_ws/src/state_lattice_planner" \
+  --volume="${PWD}:/root/catkin_ws/src/slp_doa" \
   --net='host' \
   --name="slp_doa_container" \
   slp_doa_docker \
