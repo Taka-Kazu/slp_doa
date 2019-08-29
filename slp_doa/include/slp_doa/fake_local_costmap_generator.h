73f163703eebf342364f42fdfab211d89ef0bdf2
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-08-29 13:32:06 +0900

    update for setting cost of static obstacles

--- slp_doa/include/slp_doa/fake_local_costmap_generator.h
+++ slp_doa/include/slp_doa/fake_local_costmap_generator.h
@@ -3,23 +3,36 @@
 
 #include <ros/ros.h>
 #include <tf/tf.h>
+#include <tf/transform_listener.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <geometry_msgs/PoseArray.h>
 
 class FakeLocalCostmapGenerator
 {
 public:
     FakeLocalCostmapGenerator(void);
     void process(void);
+    void obstacles_callback(const geometry_msgs::PoseArrayConstPtr&);
+
+    int get_i_from_x(const nav_msgs::OccupancyGrid&, double x);
+    int get_j_from_y(const nav_msgs::OccupancyGrid&, double y);
+    int get_index(const nav_msgs::OccupancyGrid&, double x, double y);
 
 private:
     double HZ;
     std::string ROBOT_FRAME;
     double RESOLUTION;
     double MAP_WIDTH;
+    double COLLISION_RADIUS;
+    bool ENABLE_STATIC_OBSTACLE;
 
     ros::NodeHandle nh;
     ros::NodeHandle local_nh;
     ros::Publisher map_pub;
+    ros::Subscriber obstacles_sub;
+
+    tf::TransformListener listener;
+    geometry_msgs::PoseArray obstacles;
 };
 
 #endif// __FAKE_LOCAL_COSTMAP_GENERATOR_H
--- slp_doa/src/fake_local_costmap_generator.cpp
+++ slp_doa/src/fake_local_costmap_generator.cpp
@@ -4,17 +4,42 @@ FakeLocalCostmapGenerator::FakeLocalCostmapGenerator(void)
 :local_nh("~")
 {
     local_nh.param("/dynamic_avoidance/ROBOT_FRAME", ROBOT_FRAME, {"/base_link"});
-    local_nh.param("HZ", HZ, {10});
+    local_nh.param("HZ", HZ, {20});
     local_nh.param("RESOLUTION", RESOLUTION, {0.05});
     local_nh.param("MAP_WIDTH", MAP_WIDTH, {20.0});
+    local_nh.param("ENABLE_STATIC_OBSTACLE", ENABLE_STATIC_OBSTACLE, {false});
+    local_nh.param("COLLISION_RADIUS", COLLISION_RADIUS, {0.6});
 
     map_pub = nh.advertise<nav_msgs::OccupancyGrid>("/local_costmap", 1);
+    obstacles_sub = nh.subscribe("/dynamic_obstacles", 1, &FakeLocalCostmapGenerator::obstacles_callback, this);
 
     std::cout << "=== local_costmap ===" << std::endl;
     std::cout << "ROBOT_FRAME: " << ROBOT_FRAME  << std::endl;
     std::cout << "HZ: " << HZ  << std::endl;
     std::cout << "RESOLUTION: " << RESOLUTION  << std::endl;
     std::cout << "MAP_WIDTH: " << MAP_WIDTH  << std::endl;
+    std::cout << "ENABLE_STATIC_OBSTACLE: " << ENABLE_STATIC_OBSTACLE  << std::endl;
+    std::cout << "COLLISION_RADIUS: " << COLLISION_RADIUS  << std::endl;
+}
+
+void FakeLocalCostmapGenerator::obstacles_callback(const geometry_msgs::PoseArrayConstPtr& msg)
+{
+    geometry_msgs::PoseArray obstacles_ = *msg;
+    try{
+        for(auto& p : obstacles_.poses){
+            geometry_msgs::PoseStamped p_;
+            p_.header = obstacles_.header;
+            p_.pose = p;
+            listener.waitForTransform(ROBOT_FRAME, p_.header.frame_id, p_.header.stamp, ros::Duration(1.0));
+            listener.transformPose(ROBOT_FRAME, p_, p_);
+            p = p_.pose;
+        }
+        obstacles_.header.frame_id = ROBOT_FRAME;
+        obstacles = obstacles_;
+    }catch(tf::TransformException ex){
+        std::cout << ex.what() << std::endl;
+        return;
+    }
 }
 
 void FakeLocalCostmapGenerator::process(void)
@@ -37,10 +62,42 @@ void FakeLocalCostmapGenerator::process(void)
         for(auto& data : local_costmap.data){
             data = 0;
         }
+        if(ENABLE_STATIC_OBSTACLE){
+            std::cout << "obstacles: " << obstacles.poses.size() << std::endl;
+            for(const auto& obstacle : obstacles.poses){
+                for(double y=obstacle.position.y-COLLISION_RADIUS;y<obstacle.position.y+COLLISION_RADIUS;y+=RESOLUTION){
+                    const double MAP_WIDTH_2 = MAP_WIDTH * 0.5;
+                    if(-MAP_WIDTH_2 < y && y < MAP_WIDTH_2){
+                        double x0 = obstacle.position.x - sqrt(COLLISION_RADIUS * COLLISION_RADIUS - (y - obstacle.position.y) * (y - obstacle.position.y));
+                        double x1 = obstacle.position.x + sqrt(COLLISION_RADIUS * COLLISION_RADIUS - (y - obstacle.position.y) * (y - obstacle.position.y));
+                        for(double x=x0;x<=x1;x+=RESOLUTION){
+                            if(-MAP_WIDTH_2 < x && x < MAP_WIDTH_2){
+                                local_costmap.data[get_index(local_costmap, x, y)] = 100;
+                            }
+                        }
+                    }
+                }
+            }
+        }
         map_pub.publish(local_costmap);
         ros::spinOnce();
         loop_rate.sleep();
     }
+}
+
+inline int FakeLocalCostmapGenerator::get_i_from_x(const nav_msgs::OccupancyGrid& map, double x)
+{
+    return floor((x - map.info.origin.position.x) / map.info.resolution + 0.5);
+}
+
+inline int FakeLocalCostmapGenerator::get_j_from_y(const nav_msgs::OccupancyGrid& map, double y)
+{
+    return floor((y - map.info.origin.position.y) / map.info.resolution + 0.5);
+}
+
+inline int FakeLocalCostmapGenerator::get_index(const nav_msgs::OccupancyGrid& map, double x, double y)
+{
+    return map.info.width * get_j_from_y(map, y) + get_i_from_x(map, x);
 }
 
 int main(int argc, char** argv)
