0a2667c5b1f510be5766e674c0ca33fa5655bd65
kazuki takahashi <tkhsh.schoen@gmail.com>
2020-02-13 15:44:08 +0900

    update fake_local_goal_publisher

--- slp_doa/include/slp_doa/fake_local_goal_publisher.h
+++ slp_doa/include/slp_doa/fake_local_goal_publisher.h
@@ -3,7 +3,10 @@
 
 #include <ros/ros.h>
 #include <tf/tf.h>
+#include <tf/transform_listener.h>
 #include <geometry_msgs/PoseStamped.h>
+
+#include <Eigen/Dense>
 
 class FakeLocalGoalPublisher
 {
@@ -14,13 +17,17 @@ public:
 private:
     double HZ;
     std::string WORLD_FRAME;
+    std::string ROBOT_FRAME;
+    // in world frame
     double GOAL_X;
     double GOAL_Y;
     double GOAL_YAW;
+    double GOAL_DISTANCE;
 
     ros::NodeHandle nh;
     ros::NodeHandle local_nh;
     ros::Publisher goal_pub;
+    tf::TransformListener listener;
 };
 
 #endif// __FAKE_LOCAL_GOAL_PUBLISHER_H
--- slp_doa/src/fake_local_goal_publisher.cpp
+++ slp_doa/src/fake_local_goal_publisher.cpp
@@ -4,15 +4,18 @@ FakeLocalGoalPublisher::FakeLocalGoalPublisher(void)
 :local_nh("~")
 {
     local_nh.param("/dynamic_avoidance/WORLD_FRAME", WORLD_FRAME, {"/map"});
+    local_nh.param("/dynamic_avoidance/ROBOT_FRAME", ROBOT_FRAME, {"base_link"});
     local_nh.param("HZ", HZ, {10});
     local_nh.param("GOAL_X", GOAL_X, {10.0});
     local_nh.param("GOAL_Y", GOAL_Y, {0.0});
     local_nh.param("GOAL_YAW", GOAL_YAW, {0.0});
+    local_nh.param("GOAL_DISTANCE", GOAL_DISTANCE, {5.0});
 
     goal_pub = nh.advertise<geometry_msgs::PoseStamped>("/local_goal", 1);
 
     std::cout << "=== fake_local_goal_publisher ===" << std::endl;
     std::cout << "WORLD_FRAME: " << WORLD_FRAME  << std::endl;
+    std::cout << "ROBOT_FRAME: " << ROBOT_FRAME  << std::endl;
     std::cout << "HZ: " << HZ  << std::endl;
 }
 
@@ -22,13 +25,22 @@ void FakeLocalGoalPublisher::process(void)
 
     while(ros::ok()){
         std::cout << "=== fake_local_goal_publisher ===" << std::endl;
-        geometry_msgs::PoseStamped local_goal;
-        local_goal.header.frame_id = WORLD_FRAME;
-        local_goal.header.stamp = ros::Time::now();
-        local_goal.pose.position.x = GOAL_X;
-        local_goal.pose.position.y = GOAL_Y;
-        local_goal.pose.orientation = tf::createQuaternionMsgFromYaw(GOAL_YAW);
-        goal_pub.publish(local_goal);
+        try{
+            geometry_msgs::PoseStamped local_goal;
+            local_goal.header.frame_id = WORLD_FRAME;
+            local_goal.header.stamp = ros::Time(0);
+            local_goal.pose.position.x = GOAL_X;
+            local_goal.pose.position.y = GOAL_Y;
+            local_goal.pose.orientation = tf::createQuaternionMsgFromYaw(GOAL_YAW);
+            listener.transformPose(ROBOT_FRAME, local_goal, local_goal);
+            Eigen::Vector2d goal_vector(local_goal.pose.position.x, local_goal.pose.position.y);
+            goal_vector = std::min(GOAL_DISTANCE, goal_vector.norm()) * goal_vector.normalized();
+            local_goal.pose.position.x = goal_vector(0);
+            local_goal.pose.position.y = goal_vector(1);
+            goal_pub.publish(local_goal);
+        }catch(tf::TransformException& ex){
+            std::cout << ex.what() << std::endl;
+        }
         ros::spinOnce();
         loop_rate.sleep();
     }
