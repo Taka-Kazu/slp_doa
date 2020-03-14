985e9a546f1b243bbdc8403724127010f3f0013f
kazuki takahashi <tkhsh.schoen@gmail.com>
2020-03-14 11:01:49 +0900

    add evaluation scripts

---
+++ slp_doa/scripts/result_comparer.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import csv
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+def main():
+    parser = argparse.ArgumentParser(description='read result from csv')
+    parser.add_argument('file_name_0', metavar='file_name_0', type=str, help='input file name')
+    parser.add_argument('file_name_1', metavar='file_name_1', type=str, help='input file name')
+    args = parser.parse_args()
+
+    result0 = np.loadtxt(args.file_name_0, delimiter=',')
+    result1 = np.loadtxt(args.file_name_1, delimiter=',')
+    plt.rcParams['font.family'] = 'Times new Roman'
+
+    fig = plt.figure()
+    ax0 = fig.add_subplot(2, 2, 1)
+    ax1 = fig.add_subplot(2, 2, 2)
+    ax2 = fig.add_subplot(2, 2, 3)
+    ax3 = fig.add_subplot(2, 2, 4)
+    bp0 = ax0.boxplot((result0[:, 0], result1[:, 0]), whis="range", showmeans=True, meanline=True)
+    ax0.set_xticklabels(['Proposed method', 'CADRL'])
+    ax0.set_title('(a) Travel distance[m]', y=-0.35, fontsize=10)
+    ax0.set_ylim(0, 60)
+    ax0.set_yticks(np.arange(0, ax0.get_ylim()[1]+10, 10))
+    ax0.grid()
+    print("travel distance")
+    print("P. M. mean: " + str(result0[:, 0].mean()) + "[m]")
+    print("P. M. median: " + str(np.median(result0[:, 0])) + "[m]")
+    print("CADRL mean: " + str(result1[:, 0].mean()) + "[m]")
+    print("CADRL median: " + str(np.median(result1[:, 0])) + "[m]")
+    print(stats.ttest_ind(result0[:, 0], result1[:, 0], equal_var=False))
+
+    bp1 = ax1.boxplot((result0[:, 1], result1[:, 1]), whis="range", showmeans=True, meanline=True)
+    ax1.set_xticklabels(['Proposed method', 'CADRL'])
+    ax1.set_title('(b) Travel time[s]', y=-0.35, fontsize=10)
+    ax1.set_ylim(0, 60)
+    ax1.set_yticks(np.arange(0, ax1.get_ylim()[1]+10, 10))
+    ax1.grid()
+    print("travel time")
+    print("P. M. mean: " + str(result0[:, 1].mean()) + "[s]")
+    print("P. M. median: " + str(np.median(result0[:, 1])) + "[s]")
+    print("CADRL mean: " + str(result1[:, 1].mean()) + "[s]")
+    print("CADRL median: " + str(np.median(result1[:, 1])) + "[s]")
+    print(stats.ttest_ind(result0[:, 1], result1[:, 1], equal_var=False))
+
+    bp2 = ax2.boxplot((result0[:, 2], result1[:, 2]), whis="range", showmeans=True, meanline=True)
+    ax2.set_xticklabels(['Proposed method', 'CADRL'])
+    ax2.set_title('(c) Number of collisions', y=-0.35, fontsize=10)
+    ax2.set_ylim(0, 6)
+    ax2.set_yticks(np.arange(0, ax2.get_ylim()[1]+1, 1))
+    ax2.grid()
+    print("collision count")
+    print("P. M. mean: " + str(result0[:, 2].mean()))
+    print("P. M. median: " + str(np.median(result0[:, 2])))
+    print("P. M. c. rate: " + str(np.where(result0[:, 2] > 0)[0].shape[0] / float(result0[:, 2].shape[0])))
+    print("CADRL mean: " + str(result1[:, 2].mean()))
+    print("CADRL median: " + str(np.median(result1[:, 2])))
+    print("CADRL. c. rate: " + str(np.where(result1[:, 2] > 0)[0].shape[0] / float(result1[:, 2].shape[0])))
+    print(stats.ttest_ind(result0[:, 2], result1[:, 2], equal_var=False))
+
+    bp3 = ax3.boxplot((result0[:, 3], result1[:, 3]), whis="range", showmeans=True, meanline=True)
+    ax3.set_xticklabels(['Proposed method', 'CADRL'])
+    ax3.set_title('(d) Minimum distance[m]', y=-0.35, fontsize=10)
+    ax3.set_ylim(0, 1.5)
+    ax3.set_yticks(np.arange(0, ax3.get_ylim()[1]+0.5, 0.5))
+    ax3.axhline(0.6, c="r")
+    ax3.text(2.5, 0.6, "$R_{col}$", size=10, color="red")
+    ax3.grid()
+    print("min distance")
+    print("P. M. mean: " + str(result0[:, 3].mean()) + "[m]")
+    print("P. M. median: " + str(np.median(result0[:, 3])) + "[m]")
+    print("CADRL mean: " + str(result1[:, 3].mean()) + "[m]")
+    print("CADRL median: " + str(np.median(result1[:, 3])) + "[m]")
+    print(stats.ttest_ind(result0[:, 3], result1[:, 3], equal_var=False))
+
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    plt.show()
+
+if __name__=="__main__":
+    main()
---
+++ slp_doa/scripts/velocity_result_reader.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import csv
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def main():
+    parser = argparse.ArgumentParser(description='read result')
+    parser.add_argument('dir_name', metavar='dir_name', type=str, help='input dir name')
+    args = parser.parse_args()
+    print(args.dir_name)
+
+    files = os.listdir(args.dir_name)
+    # print(files)
+
+    # results = list()
+    results = None
+    for name in files:
+        result = np.loadtxt(args.dir_name + name, delimiter=',')
+        if result.shape[0] is 0:
+            print("error in ", name)
+            continue
+        if results is not None:
+            results = np.vstack((results, result))
+        else:
+            results = result
+        # results.append(result)
+
+    # results = np.loadtxt(args.dir_name, delimiter=',')
+    print("v mean: " + str(results[:, 0].mean()))
+    print("v stddev: " + str(results[:, 0].std()))
+    print("v min: " + str(results[:, 0].min()))
+    print("v max: " + str(results[:, 0].max()))
+    print("w mean: " + str(results[:, 1].mean()))
+    print("w stddev: " + str(results[:, 1].std()))
+    print("w min: " + str(results[:, 1].min()))
+    print("w max: " + str(results[:, 1].max()))
+    print("a mean: " + str(results[:, 2].mean()))
+    print("a stddev: " + str(results[:, 2].std()))
+    print("a min: " + str(results[:, 2].min()))
+    print("a max: " + str(results[:, 2].max()))
+    print("aw mean: " + str(results[:, 3].mean()))
+    print("aw stddev: " + str(results[:, 3].std()))
+    print("aw min: " + str(results[:, 3].min()))
+    print("aw max: " + str(results[:, 3].max()))
+
+    # print("ave. traveled distance: " + str(results[:, 0].mean()) + "[m]")
+    # print("ave. traveled time: " + str(results[:, 1].mean()) + "[s]")
+    # print("ave. collision count: " + str(results[:, 2].mean()))
+
+    # fig, ax = plt.subplots()
+    # bp = ax.boxplot(results)
+    # ax.set_xticklabels(['traveled distance', 'traveled time', 'collision count', 'min distance'])
+    # plt.grid()
+    # plt.show()
+
+if __name__=="__main__":
+    main()
